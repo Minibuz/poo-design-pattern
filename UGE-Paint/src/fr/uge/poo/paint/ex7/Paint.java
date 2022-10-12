c5dc6dda69394210d3951f4444175e8c9fba09a5
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2022-10-12 16:59:06 +0200

    feat: add file selection when starting the program

--- UGE-Paint/src/fr/uge/poo/paint/ex7/Paint.java
+++ UGE-Paint/src/fr/uge/poo/paint/ex7/Paint.java
@@ -7,12 +7,26 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Paint {
+
+    /**
+     * Usage :
+     *  U need a first param as a file then u can put -legacy or nothing to select the graphics
+     *
+     * @param args
+     * @throws IOException
+     *          File cannot be read.
+     */
     public static void main(String[] args) throws IOException {
+        if(args.length<1) {
+            throw new IllegalArgumentException("first arg need to be a file name -" +
+                    "file need to be at the root of the project");
+        }
+        var path = Paths.get(args[0]);
+
         var libraryUse = LibraryAdapter.Library.coolgraphics;
-        if(args.length>0 && "-legacy".equals(args[0])) {
+        if(args.length>1 && "-legacy".equals(args[1])) {
             libraryUse = LibraryAdapter.Library.simplegraphics;
         }
-        var path = Paths.get("draw-big.txt");
         Drawing.loadFile(path, libraryUse).display();
     }
 }
