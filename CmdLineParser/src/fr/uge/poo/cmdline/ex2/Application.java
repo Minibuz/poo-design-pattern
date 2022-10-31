4d30450d25c7d38894e45666508e04de645f2079
leo.buzelin <leo.buzelin@edu.univ-eiffel.fr>
2022-10-31 01:40:03 +0100

    feat: CmdLineParser v2.0 pre-release

---
+++ CmdLineParser/src/fr/uge/poo/cmdline/ex2/Application.java
@@ -0,0 +1,48 @@
+package fr.uge.poo.cmdline.ex2;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class Application {
+
+    static private class PaintSettings {
+        private boolean legacy=false;
+        private boolean bordered=true;
+
+        public void setLegacy(boolean legacy) {
+            this.legacy = legacy;
+        }
+
+        public boolean isLegacy(){
+            return legacy;
+        }
+
+        public void setBordered(boolean bordered){
+            this.bordered=bordered;
+        }
+
+        public boolean isBordered(){
+            return bordered;
+        }
+
+        @Override
+        public String toString(){
+            return "PaintSettings [ bordered = "+bordered+", legacy = "+ legacy +" ]";
+        }
+    }
+
+    public static void main(String[] args) throws NoParameterGiven {
+        var options = new PaintSettings();
+        String[] arguments={"-legacy","-no-borders","filename1","filename2"};
+        var cmdParser = new CmdLineParser();
+        cmdParser.registerOption("-legacy", () -> options.setLegacy(true));
+        cmdParser.registerOption("-with-borders", () -> options.setBordered(true));
+        cmdParser.registerOption("-no-borders", () -> options.setBordered(false));
+        List<String> result = cmdParser.process(arguments);
+        List<Path> files = result.stream().map(Path::of).toList();
+        // this code replaces the rest of the application
+        files.forEach(System.out::println);
+        System.out.println(options.toString());
+
+    }
+}
---
+++ CmdLineParser/src/fr/uge/poo/cmdline/ex2/NoParameterGiven.java
@@ -0,0 +1,7 @@
+package fr.uge.poo.cmdline.ex2;
+
+public class NoParameterGiven extends RuntimeException {
+    NoParameterGiven(String s) {
+        super(s);
+    }
+}
